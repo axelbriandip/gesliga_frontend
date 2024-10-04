2d9814101c0ac3154ac13027094c04555cc4311c
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-10-04 18:02:03 -0300

    navigate to or reverse between panel-admin and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  BrowserRouter
+} from 'react-router-dom';
 
 // styles
 import './styles/reset.css'
@@ -8,14 +12,15 @@ import './styles/App.css'
 import Login from './pages/Login';
 import Home from './pages/Home';
 import PanelAdmin from './pages/PanelAdmin';
-// import Nav from './components/Nav';
+import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 function App() {
+
   return (
     <div className='App'>
       <BrowserRouter>
-        {/* <Nav/> */}
+        <Nav/>
           <Routes>
             <Route path='/login' element={<Login/>}/>
             <Route path='/' element={<Home/>}/>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import '../styles/nav.css';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <nav className='cnt-nav'>
+            <div className="logo">Gesliga</div>
+            <div className="info-user">
+                <div className="cnt-image">
+                    <img className='avatar' src="https://i.pinimg.com/474x/3a/13/e8/3a13e8a5ed1a65b919476338f4bf7277.jpg"/>
+                </div>
+                <div className="cnt-text">
+                    <span className='name'>Axel Brian Dip</span>
+                    <span className='role'>Administrador</span>
+                </div>
+                <div className="logout"><Link to="/login">Salir</Link></div>
+            </div>
+        </nav>
     );
 }
 
